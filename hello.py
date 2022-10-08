c8e3b37cba046c57e186f7715bae524b8a65cf20
Rimadolores <114979155+Rimadolores@users.noreply.github.com>
2022-10-08 14:51:12 +0800

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+app = Flask(_name_)
+empDB=[
+{
+'id':'101',
+'name':'Dorry Britz',
+'title':'Technical Leader'
+},
+{
+'id':'102',
+'name':'Barbie Gurl',
+'title':'Software Engineer'
+}
+]
+
+@app.route("/")
+def hello():
+	return "Hello World!"
+
+@app.route('/empdb/employee/', methods=['GET'])
+def getAllEmp():
+	return jsonify({'emp':empDB})
+
+@app.route('/empdb/employee/<empId>', methods=['GET'])
+def getEmp(empId):
+	usr = [ emp for emp in empDB if (emp['id']==empId)]
+	return jsonify({'emp':usr})
+
+if _name_ == '_main_':
+	app.run()
